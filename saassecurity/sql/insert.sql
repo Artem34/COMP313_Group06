41e8f3137a04c8e0f4a287438712da84061fae92
Harpreet <harpreet.kaur6739@gmail.com>
2018-03-24 15:38:35 -0400

    Important: First User required to run the application

--- saassecurity/sql/insert.sql
+++ saassecurity/sql/insert.sql
@@ -1,3 +1,3 @@
-﻿insert into users values('hkaur','1234','admin');
-insert into users values('manraj','1234','admin');
-insert into users values('fred','1234','admin');
+﻿insert into employees(firstName, lastName, dob, address, email, contactNum, userId) values('harpreet', 'kaur', '1991-01-08','20 Mackinac Cr','kaur@gmail.com',9999999,'kaur');
+
+insert into users(userId, password, role, empId) values('kaur','1234','admin',(select empId from employees where userId = 'kaur'));
